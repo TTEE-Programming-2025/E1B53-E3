6f26666847e9dbd94615193e8504c80c2c12a00a
joie chou <smallbookjj@gmail.com>
2025-05-13 01:34:11 +0800

    密碼忘記要驗證，for 迴圈沒有寫完整

--- E1B53.E3.c
+++ E1B53.E3.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
-int main(){
+    int main(){
+    int password,attempts=0;
     int choice;
     for(int i=0;i<20;i++){
     printf("##########################\n");
+    }
+    while(attempts<3){
+    printf("請輸入密碼(預設為2025): ");
+    scanf("%d",&password);
+    if(password==2025){
+    break;
+    }
+    attempts++;
+    if(attempts==3){
+    printf("密碼錯誤次數過多，系統結束。\n");
+    return 0;
+    }
     }
     while(1){
     printf("--------[Booking System]--------\n");
@@ -12,7 +25,12 @@ int main(){
     printf("d. Exit\n");
     printf("Enter your choice: ");
     scanf(" %c",&choice);
-    if(choice=='d'){
+    if(choice=='a'){
+    printf("\\123456789\n");
+    for(int i=8;i>=1;i--){ 
+    printf("%d-*********\n",i);
+    }
+    }else if(choice=='d'){
     break;
     }
     }
